bc498c438326f6df374251fe94508f52d0812405
Mariette Souppe <msouppe@gmail.com>
2018-05-06 14:17:08 -0700

    Setup progress

--- pipelines/ceph/setup.sh
+++ pipelines/ceph/setup.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 # [wf] execute setup.sh stage
 
+docker run --rm --name=bl \
+    -v `pwd`:/bl \
+    -v /home/yeyette/Desktop/Ceph/pipelines/ceph:/hosts \
+    -v /home/yeyette:/.ssh/id_rsa \
+    --workdir=/bl \
+    --net=host \
+    baseliner -e -s -i /hosts
