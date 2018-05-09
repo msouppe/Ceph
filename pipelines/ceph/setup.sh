f4715ff14ccdb6208d258385db640d74f474b056
Mariette Souppe <msouppe@gmail.com>
2018-05-09 10:52:00 -0700

    Update setup.sh

--- pipelines/ceph/setup.sh
+++ pipelines/ceph/setup.sh
@@ -3,8 +3,8 @@
 
 docker run --rm --name=bl \
     -v `pwd`:/bl \
-    -v /home/yeyette/Desktop/Ceph/pipelines/ceph:/hosts \
-    -v /home/yeyette:/.ssh/id_rsa \
+    -v /home/yeyette/Desktop/Ceph/pipelines/ceph/hosts:/hosts \
+    -v /home/yeyette/.ssh/id_rsa:/id_rsa \
     --workdir=/bl \
     --net=host \
     ivotron/baseliner:0.2 -e -s -i /hosts
