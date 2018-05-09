ff09e44c0979154063455ea856c4eae2161e46ec
Mariette Souppe <msouppe@gmail.com>
2018-05-09 13:12:42 -0700

    Update configs

--- pipelines/ceph/run.sh
+++ pipelines/ceph/run.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 # [wf] execute run.sh stage
 
+docker run --rm --name=bl \
+    -v `pwd`:/bl \
+    -v /home/yeyette/Desktop/Ceph/pipelines/ceph/hosts:/hosts \
+    -v /home/yeyette/.ssh/id_rsa:/root/.ssh/id_rsa \
+    --workdir=/bl \
+    --net=host \
+    ivotron/baseliner:0.2 -e -s -i /hosts
--- pipelines/ceph/setup.sh
+++ pipelines/ceph/setup.sh
@@ -1,10 +1,25 @@
 #!/usr/bin/env bash
 # [wf] execute setup.sh stage
 
-docker run --rm --name=bl \
-    -v `pwd`:/bl \
-    -v /home/yeyette/Desktop/Ceph/pipelines/ceph/hosts:/hosts \
-    -v /home/yeyette/.ssh/id_rsa:/id_rsa \
-    --workdir=/bl \
-    --net=host \
-    ivotron/baseliner:0.2 -e -s -i /hosts
+# Creates virtual network in Docker
+CEPH_NET=175.20.0.0/16
+
+netexists=`docker network ls --filter name=cephnet -q | wc -l`
+if [ $netexists -eq 0 ]; then
+  docker network create --subnet=$CEPH_NET cephnet
+fi
+
+MON_IP=175.20.0.12
+
+# Launch Nodes
+function launch_node {
+  docker run -d --name=node$1 \
+    -p 222$1:22 \
+    -e ADD_INSECURE_KEY=true \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v /tmp:/tmp \
+    ivotron/python-sshd:debian-9
+}
+
+launch_node 0
+launch_node 1
