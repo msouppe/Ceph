401c3fe873b577b76a7aaadc37a292fc39929b07
Ivo Jimenez <ivo.jimenez@gmail.com>
2018-06-06 17:58:56 -0700

    Adds automated allocation of a cluster on cloudlab

--- pipelines/ceph/geni/request.py
+++ pipelines/ceph/geni/request.py
@@ -7,48 +7,31 @@ from geni.rspec import pg as rspec
 if not os.path.isdir('/output'):
     raise Exception("expecting '/output folder'")
 
-img = "urn:publicid:IDN+apt.emulab.net+image+schedock-PG0:docker-ubuntu16:0"
+img = "urn:publicid:IDN+clemson.cloudlab.us+image+schedock-PG0:docker-ubuntu16"
 
 requests = {}
 
 
-def create_request(site, hw_type):
-    node = rspec.RawPC(hw_type)
-    node.disk_image = img
-    node.hardware_type = hw_type
+def create_request(site, hw_type, num_nodes):
 
-    if site not in requests:
-        requests[site] = rspec.Request()
+    for i in range(0, num_nodes):
 
-    requests[site].addResource(node)
+        node = rspec.RawPC('node' + str(i))
+        node.disk_image = img
+        node.hardware_type = hw_type
+
+        if site not in requests:
+            requests[site] = rspec.Request()
+
+        requests[site].addResource(node)
 
 
-create_request('apt', 'r320')
-create_request('cl-clemson', 'c6320')
-create_request('cl-clemson', 'c8220')
-create_request('cl-utah', 'm510')
-create_request('cl-wisconsin', 'c220g1')
-create_request('cl-wisconsin', 'c220g2')
-create_request('ig-utahddc', 'dl360')
-create_request('pg-kentucky', 'pc2400')
-create_request('pg-kentucky', 'pc3400')
-create_request('pg-utah', 'd710')
-create_request('pg-utah', 'd430')
-create_request('pg-utah', 'pc3000')
-
-# create_request('apt', 'r720')
-# create_request('apt', 'c6220')
-# create_request('cl-utah', 'r720')
-# create_request('pg-kentucky', 'pc3300')
-# create_request('pg-kentucky', 'pc3500')
-# create_request('pg-utah', 'd530')
-# create_request('pg-utah', 'd820')
-# create_request('pg-utah', 'd2100')
+create_request('cl-clemson', 'c6320', 3)
 
 print("Executing cloudlab request")
-manifests = cl.request(experiment_name=('quiho-'+os.environ['CLOUDLAB_USER']),
+manifests = cl.request(experiment_name=('ceph-'+os.environ['CLOUDLAB_USER']),
                        requests=requests, timeout=30, expiration=1200,
-                       ignore_failed_slivers=True)
+                       ignore_failed_slivers=False)
 
 print("Writing /output/machines file")
 with open('/output/machines', 'w') as f:
