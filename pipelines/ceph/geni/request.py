534315542b86aaea47164107f5939f38eacdec24
mariette souppe <mariette@eduroam-169-233-234-228.ucsc.edu>
2018-10-17 17:48:15 -0700

    Update request.py

--- pipelines/ceph/geni/request.py
+++ pipelines/ceph/geni/request.py
@@ -4,6 +4,8 @@ import os.path
 import geni.cloudlab_util as cl
 from geni.rspec import pg as rspec
 
+nodeCount = 3
+
 if not os.path.isdir('/output'):
     raise Exception("expecting '/output folder'")
 
@@ -11,7 +13,11 @@ img = "urn:publicid:IDN+clemson.cloudlab.us+image+schedock-PG0:docker-ubuntu16"
 
 requests = {}
 
-
+# Function: create_request(site, hw_type, num_nodes)
+# site: Location of cluster site
+# hw_type: Node type
+# num_nodes: Number of nodes to be requested
+# Node specifications for ClouLab sites: http://docs.cloudlab.us/hardware.html#%28part._cloudlab-clemson%29
 def create_request(site, hw_type, num_nodes):
 
     for i in range(0, num_nodes):
@@ -25,21 +31,35 @@ def create_request(site, hw_type, num_nodes):
 
         requests[site].addResource(node)
 
-
-create_request('cl-clemson', 'c6320', 3)
+create_request('cl-clemson', 'c6320', nodeCount)
 
 print("Executing cloudlab request")
-manifests = cl.request(experiment_name=('ceph-'+os.environ['CLOUDLAB_USER']),
+manifests = cl.request(experiment_name=('ceph1-'+os.environ['CLOUDLAB_USER']),
                        requests=requests, timeout=30, expiration=1200,
                        ignore_failed_slivers=False)
 
+# Need algorithm to determine number of mons, osds, and clients
+
+# Writing machines file and grouping allocated resources from CloudLab
 print("Writing /output/machines file")
 with open('/output/machines', 'w') as f:
     for site, manifest in manifests.iteritems():
-        for n in manifest.nodes:
+        for i,n in enumerate(manifest.nodes):
+            if i == 0:
+                f.write('[mons]' + os.linesep)
+            elif i == 1:
+                f.write(os.linesep + '[osds]' + os.linesep)
+            #elif i == (nodeCount -1)
+            #    f.write(os.linesep + '[clients]' + os.linesep)
+            
             f.write(n.hostfqdn)
             f.write(' ansible_user=' + os.environ['CLOUDLAB_USER'])
             f.write(' ansible_become=true' + os.linesep)
 
         with open('/output/{}.xml'.format(site), 'w') as mf:
             mf.write(manifest.text)
+            #mf.write(manifest.nodes[0].text)
+
+#print(manifests)
+#print(manifest.nodes[1].host['ipv4'].text)
+#print(manifest.nodes[2].host['ipv4'].text)
--- pipelines/ceph/teardown.sh
+++ pipelines/ceph/teardown.sh
@@ -1,41 +1,53 @@
 #!/bin/bash
-# [wf] remove allocated resources
-set -ex
+set -e
 
-if [ -n "$CI" ]; then
-  exit 0
-fi
+docker pull ivotron/geni-lib:v0.9.7.8 &> /dev/null
 
 if [ -z $CLOUDLAB_USER ]; then
   echo "Expecting CLOUDLAB_USER variable"
   exit 1
 fi
+
 if [ -z $CLOUDLAB_PASSWORD ]; then
   echo "Expecting CLOUDLAB_PASSWORD variable"
   exit 1
 fi
+
 if [ -z $CLOUDLAB_PROJECT ]; then
   echo "Expecting CLOUDLAB_PROJECT variable"
   exit 1
 fi
+
 if [ -z $CLOUDLAB_PUBKEY_PATH ]; then
   echo "Expecting CLOUDLAB_PUBKEY_PATH variable"
-  exit 1
-fi
-if [ -z $CLOUDLAB_CERT_PATH ]; then
-  echo "Expecting CLOUDLAB_CERT_PATH variable"
-  exit 1
+  echo "Looking in the travis secret folder"
+  if [ ! -f "geni/id_rsa.pub" ]; then
+    echo "Couldn't find id_rsa.pub"
+    exit 1
+  fi
+  echo "Found! Configuring everything for you"
+  export CLOUDLAB_PUBKEY_PATH=`pwd`/geni/id_rsa.pub
 fi
 
-# [wf] release CloudLab nodes
-docker run --rm \
+if [ -z $CLOUDLAB_CERT_PATH ]; then
+  echo "Expecting CLOUDLAB_CERT_PATH variable"
+  echo "Looking in the travis secret folder"
+  if [ ! -f "geni/cloudlab.pem" ]; then
+    echo "Couldn't find cloudlab.pem"
+    exit 1
+  fi
+  echo "Found! Configuring everything for you"
+  export CLOUDLAB_CERT_PATH=`pwd`/geni/cloudlab.pem
+fi
+
+docker run --rm --name=geni-lib \
   -e CLOUDLAB_USER=$CLOUDLAB_USER \
   -e CLOUDLAB_PASSWORD=$CLOUDLAB_PASSWORD \
   -e CLOUDLAB_PROJECT=$CLOUDLAB_PROJECT \
   -e CLOUDLAB_PUBKEY_PATH=$CLOUDLAB_PUBKEY_PATH \
   -e CLOUDLAB_CERT_PATH=$CLOUDLAB_CERT_PATH \
-  -v $CLOUDLAB_PUBKEY_PATH:$CLOUDLAB_PUBKEY_PATH:z \
-  -v $CLOUDLAB_CERT_PATH:$CLOUDLAB_CERT_PATH:z \
-  -v `pwd`/geni/release.py:/release.py:z \
+  -v $CLOUDLAB_PUBKEY_PATH:$CLOUDLAB_PUBKEY_PATH \
+  -v $CLOUDLAB_CERT_PATH:$CLOUDLAB_CERT_PATH \
+  -v `pwd`/geni/release.py:/release.py \
   --entrypoint=python \
   ivotron/geni-lib:v0.9.6.0 -u /release.py
