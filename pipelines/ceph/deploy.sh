6ed22bd75daf646b85af58c43199e1c735310775
mariette souppe <mariette@mariettes-MacBook-Air.local>
2018-11-24 00:14:55 -0800

    Update deploy.sh

--- pipelines/ceph/deploy.sh
+++ pipelines/ceph/deploy.sh
@@ -3,4 +3,7 @@ set -e
 
 docker pull mariettesouppe/ceph-ansible:v0.1
 
-docker run --name=ceph-ansible-cluster-1 souppe/ceph-ansible:v0.1 
+docker run -t --name=ceph-ansible-cluster \
+  --entrypoint=bash \
+  -v `pwd`/geni/machines:/etc/ansible/hosts \
+  mariettesouppe/ceph-ansible:v0.1 -u /etc/ansible/hosts
