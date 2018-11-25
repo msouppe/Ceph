ed63052bdbaef48e3d0bbe85d21b5acde39aa456
mariette souppe <mariette@mariettes-MacBook-Air.local>
2018-11-24 23:38:16 -0800

    Update deploy stage end entrypoint

--- pipelines/ceph/Docker/ceph-ansible/entrypoint-ceph-ansible.sh
+++ pipelines/ceph/Docker/ceph-ansible/entrypoint-ceph-ansible.sh
@@ -3,14 +3,25 @@
 git clone https://github.com/ceph/ceph-ansible.git && \
 cd ceph-ansible && \
 git checkout tags/v3.1.10 && \
-echo "Git clone and checkout complete!"
+echo "# Git clone and checkout complete!"
 
 add-apt-repository --yes --update ppa:ansible/ansible && \
-echo "Install ppa:ansible/ansible complete!"
+echo "# Install ppa:ansible/ansible complete!"
 
 apt-get update -y && \
 yes "" | apt-get install -y ansible
-echo 'Ansible install complete!'
+echo '# Ansible install complete!'
 
+cd ..
+echo `pwd`
+echo `ls`
+
+cd temp-ceph-ansible
+head -9 site-docker.yml
+mv site-docker.yml ../ceph-ansible/
+echo `pwd`
+echo `ls`
+
+cd ../ceph-ansible
 echo `pwd`
 echo `ls`
