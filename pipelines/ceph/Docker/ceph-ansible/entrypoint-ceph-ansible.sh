f43810bfc2fd68adff6e6dabb38bea875fd639a3
mariette souppe <mariette@mariettes-MacBook-Air.local>
2018-11-24 22:38:15 -0800

    Update deploy.sh

--- pipelines/ceph/Docker/ceph-ansible/entrypoint-ceph-ansible.sh
+++ pipelines/ceph/Docker/ceph-ansible/entrypoint-ceph-ansible.sh
@@ -3,6 +3,14 @@
 git clone https://github.com/ceph/ceph-ansible.git && \
 cd ceph-ansible && \
 git checkout tags/v3.1.10 && \
+echo "Git clone and checkout complete!"
+
 add-apt-repository --yes --update ppa:ansible/ansible && \
-apt-get update --yes && \
+echo "Install ppa:ansible/ansible complete!"
+
+apt-get update -y && \
 yes "" | apt-get install -y ansible
+echo 'Ansible install complete!'
+
+echo `pwd`
+echo `ls`
--- pipelines/ceph/deploy.sh
+++ pipelines/ceph/deploy.sh
@@ -5,5 +5,5 @@ docker pull mariettesouppe/ceph-ansible:v0.1
 
 docker run --rm -t --name=ceph-ansible-cluster \
   -v `pwd`/geni/machines:/etc/ansible/hosts \
-  -v `pwd`/ceph-ansible/*:/ceph-ansible \
+  -v `pwd`/ceph-ansible/site-docker.yml:/ceph-ansible/ \
   mariettesouppe/ceph-ansible:v0.2
