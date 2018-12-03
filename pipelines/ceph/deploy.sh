c58945f6edc593867d7774611eca81bbe083eac5
mariette souppe <mariette@eduroam-169-233-136-180.ucsc.edu>
2018-12-03 15:05:19 -0800

    Mariette config

--- pipelines/ceph/deploy.sh
+++ pipelines/ceph/deploy.sh
@@ -12,4 +12,4 @@ docker run --rm --name=ceph-ansible \
   -v `pwd`/geni/machines:/etc/ansible/hosts \
   -v `pwd`/ceph-ansible/site-docker.yml:/ceph-ansible/site.yml \
   -v `pwd`/ceph-ansible/group_vars:/ceph-ansible/group_vars \
-  local/ceph-ansible
+  mariettesouppe/ceph-ansible:v0.2
