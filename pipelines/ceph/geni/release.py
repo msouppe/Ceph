8f62d831342f51c5531d400b0d71a778947e49f0
Ivo Jimenez <ivo.jimenez@gmail.com>
2018-12-03 10:29:56 -0700

    More cleanup

--- pipelines/ceph/geni/release.py
+++ pipelines/ceph/geni/release.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python
 import os
 import geni.cloudlab_util as cl
-cl.release('clb-'+os.environ['CLOUDLAB_USER'])
+cl.release('ceph-'+os.environ['CLOUDLAB_USER'])
--- pipelines/ceph/teardown.sh
+++ pipelines/ceph/teardown.sh
@@ -1,43 +1,25 @@
 #!/bin/bash
 set -e
 
-docker pull ivotron/geni-lib:v0.9.7.8 &> /dev/null
-
 if [ -z $CLOUDLAB_USER ]; then
   echo "Expecting CLOUDLAB_USER variable"
   exit 1
 fi
-
 if [ -z $CLOUDLAB_PASSWORD ]; then
   echo "Expecting CLOUDLAB_PASSWORD variable"
   exit 1
 fi
-
 if [ -z $CLOUDLAB_PROJECT ]; then
   echo "Expecting CLOUDLAB_PROJECT variable"
   exit 1
 fi
-
 if [ -z $CLOUDLAB_PUBKEY_PATH ]; then
   echo "Expecting CLOUDLAB_PUBKEY_PATH variable"
-  echo "Looking in the travis secret folder"
-  if [ ! -f "geni/id_rsa.pub" ]; then
-    echo "Couldn't find id_rsa.pub"
-    exit 1
-  fi
-  echo "Found! Configuring everything for you"
-  export CLOUDLAB_PUBKEY_PATH=`pwd`/geni/id_rsa.pub
+  exit 1
 fi
-
 if [ -z $CLOUDLAB_CERT_PATH ]; then
   echo "Expecting CLOUDLAB_CERT_PATH variable"
-  echo "Looking in the travis secret folder"
-  if [ ! -f "geni/cloudlab.pem" ]; then
-    echo "Couldn't find cloudlab.pem"
-    exit 1
-  fi
-  echo "Found! Configuring everything for you"
-  export CLOUDLAB_CERT_PATH=`pwd`/geni/cloudlab.pem
+  exit 1
 fi
 
 docker run --rm --name=geni-lib \
@@ -50,4 +32,4 @@ docker run --rm --name=geni-lib \
   -v $CLOUDLAB_CERT_PATH:$CLOUDLAB_CERT_PATH \
   -v `pwd`/geni/release.py:/release.py \
   --entrypoint=python \
-  ivotron/geni-lib:v0.9.6.0 -u /release.py
+  ivotron/geni-lib:v0.9.7.9 -u /release.py
